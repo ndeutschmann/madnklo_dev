15159cd9b4d7307a7f69b3698d151ea4c9e035ec
Nicolas DEUTSCHMANN <ndeutsch@MacBook-Pro-de-Nicolas-2.local>
2019-08-31 17:40:12 +0200

    first properly working version

--- madnklo_docker.sh
+++ madnklo_docker.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" = "--docker_build" ]
 then
-	docker build --tag=madnklo_dev ./docker_src
+	docker build --tag=ndeutschmann/madnklo_dev ./docker_src
 	echo ""
 	echo "================================================================"
 	echo "   New image built. Consider pruning/removing dangling images"
@@ -12,21 +12,38 @@ fi
 
 if [ "$1" = "--docker_update" ]
 then
-	docker pull madnklo_dev
+	docker pull ndeutschmann/madnklo_dev
 	exit
 fi
 
 if [ "$1" = "--initialize" ]
 then
-	echo "running the initialization script"
+	echo "Running the initialization script"
 	docker run --rm\
 	--entrypoint=/home/hep/initialize.sh \
 	-v $(pwd)/code:/home/hep/madnklo\
-	madnklo_dev
+	ndeutschmann/madnklo_dev
+	echo "Set up the MadNkLO development environment at $(pwd)/code"
 	exit
 fi
-	
+
+if [ "$1" = "--setup_venv" ]
+then
+	echo "Setting up 'madnklo_venv' virtual environment"
+	virtualenv madnklo_venv
+	source ./madnklo_venv/bin/activate
+	./docker_src/requirements_pip.sh
+	source ./madnklo_venv/bin/deactivate
+	exit
+fi
+
+echo "MadNkLO development environment"
+echo ""
+echo "Make sure that you have properly initialized the environment by running"
+echo "madnklo_docker.sh --initialize"
+read -t 30 -p  "[press enter (30s)]"
+
 docker run -it --rm \
 -v $(pwd)/code:/home/hep/madnklo \
 -v $(pwd)/madnklo_persistent:/var/madnklo_persistent \
-madnklo_dev $1
+ndeutschmann/madnklo_dev $1
